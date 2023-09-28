735a4b00323ff330273681fa2c99c98875ed3b9e
josevazquez99 <jv319069@gmail.com>
2023-09-28 12:08:43 +0200

    calculadora

---
+++ HolaMundo/src/main/java/utility/Persona.java
@@ -0,0 +1,66 @@
+package utility;
+
+import java.util.Objects;
+
+public class Persona {
+	private String nombre;
+	private String apellidos;
+	
+	
+	public Persona(String nombre, String apellidos) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(apellidos, nombre);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(apellidos, other.apellidos) && Objects.equals(nombre, other.nombre);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellidos=" + apellidos + "]";
+	}
+	
+	
+
+	
+	
+
+}
